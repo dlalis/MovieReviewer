45a24046e82bd7ffd9ef83e3bd8634a270ce3a06
John <giannhs632@gmail.com>
2021-01-02 17:10:40 +0200

    made it responsive

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ if (session_status() == PHP_SESSION_NONE)
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Reviewer</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="css/login.css">
     <script type="text/javascript" src="js/main.js"></script>
   </head>
   <body>
@@ -17,7 +17,7 @@ if (session_status() == PHP_SESSION_NONE)
     <!-- Tabs Titles -->
 
     <!-- Login Form -->
-    <form id="form_id" action="./user.php" method="POST"> 
+    <form id="form_id" action="./home.html" method="POST"> 
        <!-- <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
       <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">-->
      <div id="username_wrapper" class="input_wrapper">
